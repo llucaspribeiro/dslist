13487be44ce32b03c35d6a67ddc2008fab063be4
llucaspribeiro <llucasp.ribeiro@hotmail.com>
2023-05-10 14:40:05 -0300

    Query SQL in Spring Data JPA and Projections

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -9,6 +10,7 @@ public class GameMinDTO {
     private Integer year;
     private String imgUrl;
     private String shortDescription;
+    private Integer position;
 
     public GameMinDTO() {
     }
@@ -20,6 +22,16 @@ public class GameMinDTO {
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+        position = projection.getPosition();
+    }
+
 
     public Long getId() {
         return id;
